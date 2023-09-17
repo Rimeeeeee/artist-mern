593e93438058aa69d99c06d339c962f3bb23f8c3
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2023-09-18 01:40:37 +0530

    bugs fixed

--- api/models/Order.js
+++ api/models/Order.js
@@ -2,9 +2,10 @@ const mongoose=require('mongoose');
 const orderSchema=new mongoose.Schema({
   product:{type:mongoose.Schema.Types.ObjectId,required:true,ref:'Product'},
   user:{type:mongoose.Schema.Types.ObjectId,required:true},
-  items:{type:Number,required:true},
+  
   home_address:{type:String,required:true},
   contact_no:{type:String,required:true},
+  items:{type:Number,required:true},
   price:Number
 });
 const OrderModel=mongoose.model('Order',orderSchema);
