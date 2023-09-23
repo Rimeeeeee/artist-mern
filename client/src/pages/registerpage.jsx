64d42a9bd947bcc5851f520014f8ec554c6dd759
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2023-09-23 10:37:04 +0530

    Update registerpage.jsx

--- client/src/pages/registerpage.jsx
+++ client/src/pages/registerpage.jsx
@@ -5,13 +5,16 @@ export default function registerpage(){
     const [name,setName]=useState('');
     const [email,setEmail]=useState('');
     const [password,setPassword]=useState('');
+    const [govno,setGovno]=useState('');
     async function registerUser(ev){
            ev.preventDefault();   
            try{
             await axios.post('/register',{
                 name,
                 email,
+                govno,
                 password
+               
             });
             alert('Registration Successful.Now you can login');
            } 
@@ -23,11 +26,12 @@ export default function registerpage(){
     return(
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
-            <h1 className="text-4xl text-center mb-4">Register</h1>
+            <h1 className="text-4xl text-center mb-4">Register portal for Artist / Customer</h1>
             <form className="max-w-md mx-auto " onSubmit={registerUser}>
             <input type="text" className="w-full border my-2 py-2 px-3 rounded-2xl"placeholder='username' value={name} onChange={ev=>setName(ev.target.value)}/>
                 <input type="email" className="w-full border my-2 py-2 px-3 rounded-2xl"placeholder='you@email.com' value={email} onChange={ev=>setEmail(ev.target.value)}/>
                 <input type="password" className="w-full border my-2 py-2 px-3 rounded-2xl"placeholder='password' value={password} onChange={ev=>setPassword(ev.target.value)}/>
+                <input type="text" className="w-full border my-2 py-2 px-3 rounded-2xl"placeholder='Write your gov id (Only for artist Not required for Customers)' value={govno} onChange={ev=>setGovno(ev.target.value)}/>
                 <button className="bg-sky-600 active:bg-blue-400 hover:bg-sky-500 p-2 w-full text-white rounded-2xl">Register</button>
                 <div className='text-center py-2 text-gray-500'>
                     Already an Member?
