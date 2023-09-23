0c7861f11620416e890242e985328e41cea1523b
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2023-09-23 10:30:36 +0530

    Update buying.jsx

--- client/src/pages/buying.jsx
+++ client/src/pages/buying.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+  import { useParams } from "react-router-dom";
 import { useState } from "react";
 import { Navigate } from "react-router-dom";
 export default function BuyingPage(){
