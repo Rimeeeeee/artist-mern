66dbf56087c8545c07bc13bd56c634931e448c1d
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2023-09-23 10:38:01 +0530

    Update accountnav.jsx

--- client/src/accountnav.jsx
+++ client/src/accountnav.jsx
@@ -7,16 +7,16 @@ export default function AccountNav(){
     }
     function linkClasses(type=null){
         let classes= 'py-2 px-6';
-        if(type===subpage){
+        /*if(type===subpage){
             classes+='bg-primary text-primary rounded-full';
-        }
+        }*/
         return classes;
     }
     return(
         <nav className="flex justify-center m-2 gap-2 nav_bar ">
             <div className="px-2 border-r border-gray-300"><Link className={linkClasses('products')} to={'/'}>Home</Link></div>
             <div className="px-2 border-r border-gray-300"><Link className={linkClasses('profile') } to={'/account'}>My Profile</Link></div> 
-           <div className="px-2 border-r border-gray-300"><Link className={linkClasses('orders')} to={'/account/orders'}>My Orders</Link></div>     
+            
            <div className="px-2 border-r border-gray-300"><Link className={linkClasses('products')} to={'/account/products'}>My Products</Link></div>
             </nav>
     );
